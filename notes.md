25a5d10e23de3c3fe0cb826dc4a6de7c6efd1635
bruol <lorin@urbantat.eu>
2021-05-14 08:01:53 +0200

    reseults swapped and put into csv

--- notes.md
+++ notes.md
@@ -1,5 +1,7 @@
-
-- LDP - 
-  - /wiki/%E8%87%AA%E7%94%B1%E6%B0%91%E4%B8%BB%E5%85%9A_(%E6%97%A5%E6%9C%AC)
-- hokkaido bezirk 
-  - https://ja.wikipedia.org/wiki/%E5%8C%97%E6%B5%B7%E9%81%93%E7%AC%AC3%E5%8C%BA
+13-16
+14-13
+15-16
+16-17
+17-18
+18-19
+19-14
---
+++ switch.py
@@ -0,0 +1,23 @@
+import json
+
+s_file = open('notes.md', 'r')
+
+f_data = open('results.json')
+data = json.load(f_data)
+f_data.close()
+
+data_dest = data
+
+
+for line in s_file:
+    pdf,wiki = line.split('-')
+    wiki = wiki.split('\n')[0]
+    prefecture_wiki = data[wiki]
+    data_dest[pdf] = prefecture_wiki
+
+results = open('results_swapped.json', "w")
+
+results.write(json.dumps(data_dest, sort_keys=False, indent=4))
+
+s_file.close()
+results.close()
